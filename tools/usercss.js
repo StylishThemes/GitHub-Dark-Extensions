3be04d61c7f5d5489f40fbd7c1d881db5a33b8db
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2020-07-08 23:03:28 +0100

    fix, usercss generation closes #1

--- tools/usercss.js
+++ tools/usercss.js
@@ -18,6 +18,10 @@ const replacements = [
   {from: /\s+\/\* usercss build - remove start[\s\S]+usercss build - remove end \*\/$/m, to: ""},
 ];
 
+const defaults = {
+  color: "#4f8cc9",
+};
+
 function replaceForUsercss(css) {
   for (const replacement of replacements) {
     css = css.replace(replacement.from, replacement.to);
@@ -25,10 +29,23 @@ function replaceForUsercss(css) {
   return css;
 }
 
+function replaceVars(css) {
+  Object.keys(defaults).forEach(key => {
+    css = css.replace(`{{${key}}}`, defaults[key]);
+  });
+  const version = css.match(/github\sdark\sextensions\sv([\d.]+) \(/i);
+  if (version) {
+    css = css.replace("{{version}}", version[1]);
+  }
+  return css;
+}
+
 async function main() {
-  let css = await readFile(files.source, "utf8");
+  let meta = await readFile(files.template, "utf8");
+  let css = await readFile(files.source, "utf8")
   css = replaceForUsercss(css);
 
+  css = replaceVars(`${meta}${css}`);
   await writeFile(files.usercss, css);
 
   console.info("\u001B[32m%s\u001B[0m", "GitHub Dark Extensions usercss build complete");
